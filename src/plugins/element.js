9269f4f716f37db507f4b20eb4f796c54feb3c3d
2531800823 <2531800823@qq.com>
2021-09-29 00:00:48 +0800

    data

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -29,9 +29,15 @@ import {
     Tree,
     Select,
     Option,
-    Cascader
+    Cascader,
+    Alert,
+    Tabs,
+    TabPane,
 } from 'element-ui';
 
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Alert)
 Vue.use(Cascader)
 Vue.use(Option)
 Vue.use(Select)
--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import User from './components/user/Users.vue'
 import Rights from './components/power/Rigths.vue'
 import Roles from './components/power/Roles.vue'
 import Cate from './components/goods/Cate.vue'
+import Params from './components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
@@ -24,7 +25,8 @@ const router  =  new VueRouter({
                 { path: '/users', component: User },
                 {path:"/rights",component:Rights},
                 { path: "/roles", component: Roles },
-                {path: '/categories', component: Cate }
+                {path: '/categories', component: Cate },
+                {path: '/params', component: Params },
                 
             ]
         },
